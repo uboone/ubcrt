f48e78bd65cde5a442c685abd60459d6dc37f8ab
Christopher Barnes <cbarnes4@uboonebuild01.fnal.gov>
2018-09-30 19:43:57 -0500

    changes to the CRT/flash beam filter

--- ubcrt/UBCRTCosmicFilter/UBCRTCosmicFilter_module.cc
+++ ubcrt/UBCRTCosmicFilter/UBCRTCosmicFilter_module.cc
@@ -15,6 +15,7 @@
 #include "art/Framework/Principal/SubRun.h"
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Services/Optional/TFileService.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
 // declare the package with the CRT hit information.
@@ -22,7 +23,11 @@
 #include "lardataobj/RecoBase/OpFlash.h"
 #include "ubobj/RawData/DAQHeaderTimeUBooNE.h"
 
+// C++
 #include <memory>
+
+// ROOT
+#include <TTree.h>
 
 class UBCRTCosmicFilter;
 
@@ -39,6 +44,9 @@ public:
   UBCRTCosmicFilter & operator = (UBCRTCosmicFilter const &) = delete;
   UBCRTCosmicFilter & operator = (UBCRTCosmicFilter &&) = delete;
 
+  // Use the 'beginJob()' function to save information in the TTree.
+  void beginJob() override;
+
   // Required functions.
   bool filter(art::Event & e) override;
 
@@ -53,15 +61,35 @@ private:
   double      fPEMin;
   double      fDTOffset;
   double      fResolution;
-  bool        fSingleBeamgateFlashOnly;
   bool        verbose;
 
-  // beam info
-  double      beam_flash_time;
-  double      beam_flash_PEs;
-  
-  // CRT hit time.
-  double      closest_CRT_hit_time;
+  // TTree Declaration.
+  TTree*      _tree;
+
+  // Event info.
+  int         _run;
+  int         _subrun;
+  int         _event;
+
+  // Beam flash info.
+  int         _nflashes_in_beamgate;
+  int         _nflashes_in_beamgate_passing_beamspill_and_PE_cuts;
+  double      _beam_flash_time;
+  double      _beam_flash_PE;
+
+  // CRT hit info.
+  int         _nCRThits_in_event;
+  double      _CRT_hit_time;
+  double      _CRT_hit_PE;
+  double      _CRT_hit_x;
+  double      _CRT_hit_y;
+  double      _CRT_hit_z;
+
+  // Comparison between CRT hit time & CRT hit PE.
+  double     _dt;
+
+  // Is the closest CRT hit within resolution?  Binary flag.
+  int        _within_resolution;
   
   // Counter for the events.
   int    event_counter;
@@ -82,18 +110,48 @@ UBCRTCosmicFilter::UBCRTCosmicFilter(fhicl::ParameterSet const & p)
   fPEMin                   = 50.0;
   fDTOffset                = 68600.0;
   fResolution              = 1.0;
-  fSingleBeamgateFlashOnly = true;
   verbose                  = true;
+}
+
+void UBCRTCosmicFilter::beginJob() 
+{
+
+  art::ServiceHandle<art::TFileService> tfs;
+  _tree = tfs->make<TTree>("_tree","Flash/CRT Matching Information For All Events");
+  _tree->Branch("_run", &_run, "run/I");
+  _tree->Branch("_subrun",&_subrun, "subrun/I");
+  _tree->Branch("_event", &_event, "event/I");
+  _tree->Branch("_nflashes_in_beamgate", &_nflashes_in_beamgate, "nflashes_in_beamgate/I");
+  _tree->Branch("_nflashes_in_beamgate_passing_beamspill_and_PE_cuts", &_nflashes_in_beamgate_passing_beamspill_and_PE_cuts, "nflashes_in_beamgate_passing_beamspill_and_PE_cuts/I");
+  _tree->Branch("_beam_flash_time", &_beam_flash_time, "beam_flash_time/D");
+  _tree->Branch("_beam_flash_PE", &_beam_flash_PE, "beam_flash_PE/D");
+  _tree->Branch("_nCRThits_in_event", &_nCRThits_in_event, "_nCRThits_in_event/I");
+  _tree->Branch("_CRT_hit_time", &_CRT_hit_time, "CRT_hit_time/D");
+  _tree->Branch("_CRT_hit_PE", &_CRT_hit_PE, "CRT_hit_PE/D");
+  _tree->Branch("_CRT_hit_x", &_CRT_hit_x, "CRT_hit_x/D");
+  _tree->Branch("_CRT_hit_y", &_CRT_hit_y, "CRT_hit_y/D");
+  _tree->Branch("_CRT_hit_z", &_CRT_hit_z, "CRT_hit_z/D");
+  _tree->Branch("_dt", &_dt, "dt/D");
+  _tree->Branch("_within_resolution", &_within_resolution, "within_resolution/I");
+
 }
 
 bool UBCRTCosmicFilter::filter(art::Event & e)
 {
   // Implementation of required member function here.
 
-  std::cout << "Now looping over event #" << event_counter << "." << std::endl;
+  // Fill the event info.
+  _event  = e.event();
+  _subrun = e.subRun();
+  _run    = e.run();
+
+  if ( verbose )
+    std::cout << "Now looping over event #" << event_counter << "." << std::endl;
+
+  // Iterate the event counter.
   event_counter++;
 
-  // Declare an object for the 
+  // Declare an object for the GPS timestamp of the event so that you can offset the cosmic t0 times.
   art::Handle< raw::DAQHeaderTimeUBooNE > rawHandle_DAQHeader;
   e.getByLabel(fDAQHeaderProducer, rawHandle_DAQHeader);
 
@@ -117,31 +175,34 @@ bool UBCRTCosmicFilter::filter(art::Event & e)
 
   // make sure beam flashes look good.                                                                                                                                                                  
   if(!beamflash_h.isValid()) {
-    std::cerr<<"\033[93m[ERROR]\033[00m ... could not locate Beam Flash!"<<std::endl;
+    std::cerr<<"\033[93m[ERROR]\033[00m ... could not locate beam flash!"<<std::endl;
     throw std::exception();
   }
 
+  // Set the variable for the number of flashes reconstructed in the beamgate for each event.
+  _nflashes_in_beamgate                                  = beamflash_h->size();
+  _nflashes_in_beamgate_passing_beamspill_and_PE_cuts    = 0; 
+
   // Loop through the beam flashes and find the closest crt hit to the beam flashes.                                                                                                                    
   if ( verbose ) 
-    std::cout << "Number of beam flashes in this event = " << beamflash_h->size() << "." << std::endl;
+    std::cout << "Number of beam flashes in this event = " << _nflashes_in_beamgate << "." << std::endl;
 
-  double closest_CRT_diff     = 100000.0;
-  closest_CRT_hit_time        = 0.;
+  // Set the variables for the closest CRT hit time.
+  double _dt_abs              = 100000.0;
+  _dt                         = 0.;
+  _CRT_hit_time               = 0.;
 
-  bool within_resolution = false;
+  // Set the variable for if the CRT hit is within resolution of the flash.
+  _within_resolution          = 0;
 
-  for ( size_t i = 0; i < beamflash_h->size(); i++ ) {
+  // Set the value for the '_beam_flash_PE' to be negative so that it will be overwritten by the information for the actual flash.
+  _beam_flash_PE = -1.0;
 
-    beam_flash_time = beamflash_h->at( i ).Time();
-    beam_flash_PEs  = beamflash_h->at( i ).TotalPE();
+  // Loop over the flashes to find the one greatest in intensity.
+  for ( int i = 0; i < _nflashes_in_beamgate; i++ ) {
 
-    if ( verbose ) {
-      std::cout << "Time of the beam flash = " << beam_flash_time << " us." << std::endl;
-      std::cout << "PEs of the beam flash = " << beam_flash_PEs << " PEs." << std::endl;
-    }
-
-    // Only look at those flashes that are in the beamgate and of the minimum amount of PEs.
-    if ( beam_flash_time < fBeamStart || beam_flash_time > fBeamEnd || beam_flash_PEs < fPEMin ) {
+    // Throw the flash out if it is outside of the beamgate or too low in intensity.
+    if ( beamflash_h->at( i ).Time() < fBeamStart || beamflash_h->at( i ).Time() > fBeamEnd || beamflash_h->at( i ).TotalPE() < fPEMin ) {
       
       if ( verbose ) 
 	std::cout << "Flash is outside of beam gate or too low in intensity.  Skipping!" << std::endl;
@@ -150,48 +211,69 @@ bool UBCRTCosmicFilter::filter(art::Event & e)
     
     }
 
-    // Loop over the CRT hits.                                                                                                                                                                           
-    for ( size_t j = 0; j < crthit_h->size(); j++ ) {
+    // Increment '_nflashes_in_beamgate_passing_beamspill_and_PE_cuts'.
+    _nflashes_in_beamgate_passing_beamspill_and_PE_cuts++;
 
-      if ( verbose ) 
-	std::cout << "Time of the CRT Hit wrt the event timestamp = " << ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec + fDTOffset ) / 1000. ) << " us." << std::endl;
+    if ( beamflash_h->at( i ).TotalPE() > _beam_flash_PE ) {
+      _beam_flash_time = beamflash_h->at( i ).Time();
+      _beam_flash_PE   = beamflash_h->at( i ).TotalPE();
 
-      // Reset the variables.                                                                                                                                                                           
-      if ( fabs( beam_flash_time - ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec  + fDTOffset ) / 1000. ) ) < closest_CRT_diff ) {
-        closest_CRT_diff     = fabs( beam_flash_time - ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec + fDTOffset ) / 1000. ) );
-        closest_CRT_hit_time = ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec +  fDTOffset ) / 1000. );
+    }
 
-	// set 'within_resolution' to 'true' and break the loop if 'closest_crt_diff' is less than fResolution.
-	if ( closest_CRT_diff < fResolution ) {
-	  within_resolution = true;
-	  break;
+  } // End of the loop over the beam flashes.
+
+  if ( verbose )
+    std::cout << "Number of flashes in the event passing the beamspill and PE cuts = " << _nflashes_in_beamgate_passing_beamspill_and_PE_cuts << "." << std::endl;
+
+  // Set the variable for the number of CRT hits in the event.
+  _nCRThits_in_event = crthit_h->size();
+
+  // Loop over the CRT hits.                                                                                                                                                                           
+  for ( int j = 0; j < _nCRThits_in_event; j++ ) {
+
+    if ( verbose ) 
+      std::cout << "Time of the CRT Hit wrt the event timestamp = " << ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec + fDTOffset ) / 1000. ) << " us." << std::endl;
+    
+    // Reset the variables.                                                                                                                                                                           
+    if ( fabs( _beam_flash_time - ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec  + fDTOffset ) / 1000. ) ) < _dt_abs ) {
+      _dt_abs       = fabs( _beam_flash_time - ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec + fDTOffset ) / 1000. ) );
+      _dt           = _beam_flash_time - ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec + fDTOffset ) / 1000. );
+      _CRT_hit_time = ( ( crthit_h->at( j ).ts0_ns - evt_timeGPS_nsec +  fDTOffset ) / 1000. );
+
+      // set 'within_resolution' to 'true' and break the loop if 'closest_crt_diff' is less than fResolution.
+      if ( _dt_abs < fResolution ) {
+	_within_resolution = 1;
+
+	// Set the position information and the intensity of the CRT hit.
+	_CRT_hit_PE = crthit_h->at( j ).peshit;
+	_CRT_hit_x  = crthit_h->at( j ).x_pos;
+	_CRT_hit_y  = crthit_h->at( j ).y_pos;
+	_CRT_hit_z  = crthit_h->at( j ).z_pos;
+
+	if ( verbose ) {
+	  std::cout << "CRT hit PE = " << _CRT_hit_PE << " PEs." << std::endl;
+	  std::cout << "CRT hit x = " << _CRT_hit_x << " cm." << std::endl;
+	  std::cout << "CRT hit y = " << _CRT_hit_y << " cm." << std::endl;
+	  std::cout << "CRT hit z = " << _CRT_hit_z << " cm." << std::endl;
 	}
+	  
 
-      } // End of conditional for closest CRT hit time.                                                                                                                                                   
+	break;
+      }
+      
+    } // End of conditional for closest CRT hit time.                                                                                                                                                   
+    
+  } // End of loop over CRT hits.                                                                                                                                                                         
 
-    } // End of loop over CRT hits.                                                                                                                                                                         
-    // End the loop over beam flashes if 'within_resolution' = 'true'.  You do not have to consider any more flashes if that is the case.
-    if ( within_resolution == true ) 
-      break;
+  // Fill the tree.
+  _tree->Fill();
 
-  } // End of loop over beam flashes.                                                                                                                                                                       
-  
-  // Look at cases in which you could return true.
-  if ( within_resolution == true ) {
+  // Return true if you are within resolution.
+  if ( _within_resolution == 1 )
+    return false;
 
-    // If you want a single flash and there is more than one in the beamgate, than return false.
-    if ( fSingleBeamgateFlashOnly == true and beamflash_h->size() != 1 ) 
-      return true;
-
-    // In all other cases return true.
-    else
-      return false;
-
-  }
-
-  // If it is not within resolution, return false.
-  else 
-    return true;
-
+  // Otherwise return false.
+  return true;
+    
 } // End of the filter module
 DEFINE_ART_MODULE(UBCRTCosmicFilter)
