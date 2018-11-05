3ff4fcee3f23a122d5afcae130dc616d9c0f7797
Rui <ran4@hawk.iit.edu>
2018-11-05 15:37:16 -0600

    backing up merge.C

--- ubcrt/CRT/test/merge.C
+++ ubcrt/CRT/test/merge.C
@@ -7,9 +7,17 @@
 
 void merge(){
 
+  TFile *outroot = new TFile("strip_comp_test.root","recreate");
+
   gROOT->SetBatch(kTRUE);
 
-  std::cout<<"so fucked up"<<std::endl;  
+  gROOT->ForceStyle(0);
+
+  gStyle->SetStatY(0.95);
+  gStyle->SetStatX(0.3);
+  gStyle->SetStatW(0.2);
+  gStyle->SetStatH(0.2); 
+  gStyle->SetOptStat(111);
 
   std::vector<int> key_bottom { 11,12,14,17,18,19,22,23,24 }; 
   std::vector<int> key_ft     { 26,27,28,29,30,31,52,56,57,58,59,60,61 }; 
@@ -25,8 +33,18 @@ void merge(){
   
   int n_hist_generated = 0 ;
 
+  TDirectory *cdtof = outroot->mkdir("CRT");
+  cdtof->cd();
+
+  TDirectory *cdplane[4];  
+  //for (int plane=0; plane<4; ++plane){
+  //cdplane[plane] = cdtof->mkdir(Form("%s,",path[plane].c_str()));
+  //}
+
   for (int plane=0; plane<4; ++plane){
     int key_feb=0;
+    cdplane[plane] = cdtof->mkdir(Form("%s,",path[plane].c_str()));
+    cdplane[plane]->cd();
     //Bottom
     for(auto feb_id : key_planes[plane]){
       key_feb=feb_id*100;
@@ -36,7 +54,10 @@ void merge(){
 	std::string s = std::to_string(strip_id);
 	char const *id_char = s.c_str();
 
-	std::cout<<strip_id<<std::endl;
+	//std::cout<<strip_id<<std::endl;
+
+	TCanvas *c_out = new TCanvas(id_char,"strip",1600,500);
+	c_out->Divide(3,1);//,0.1,0);
 	
 	for (int ax=0; ax<3; ++ax){
 	  char ax_label[] = "_x";
@@ -46,26 +67,32 @@ void merge(){
 	  std::strcpy(title,id_char);
 	  std::strcat(title,ax_label);
 	  
-
+	  c_out->cd(ax+1);
 
 	  TH1F * hist_data= (TH1F*)   _file_data->Get(Form("crt/%s/%s",path[plane].c_str(),title));
-	  TH1F * hist_mc  = (TH1F*)   _file_mc  ->Get(Form("crt/%s/%s",path[plane].c_str(),title));
-	  
-	  TCanvas *c_out = new TCanvas(title,"strip",500,500);
-	  c_out->cd();
+	  //c_out->cd();
 	  hist_data->Draw();
+
+	  TH1F * hist_mc  = (TH1F*)   _file_mc  ->Get(Form("crt/%s/%s",path[plane].c_str(),title));
+	  //c_out->cd();
 	  hist_mc->SetLineColor(kRed);
 	  hist_mc->Draw("same");
-
-	  auto legend = new TLegend(0.79,0.64,0.89,0.74);
-	  //legend->SetHeader(title,"C");
+	  
+	  auto legend = new TLegend(0.1,0.64,0.21,0.74);
 	  legend->AddEntry(hist_data,"Data","l");
 	  legend->AddEntry(hist_mc  ,"MC"  ,"l");
 	  legend->Draw();
-	  
-	  c_out->SaveAs(Form("%s/%s.pdf", path[plane].c_str(),title));
+
+	  //c_out->Update();
+	  //c_out->SaveAs(Form("%s/%s.pdf", path[plane].c_str(),title));
 	}//per ub plane
+	c_out->Draw();
+	c_out->Write();
       }//per strip
     }//per feb
   }//per CRT plane
+  outroot->Write();
+  //outroot->Close();
+  std::cout<<"Generated "<<n_hist_generated<<"/1168 histograms !"<<std::endl;
+  //delete outroot;
 }
