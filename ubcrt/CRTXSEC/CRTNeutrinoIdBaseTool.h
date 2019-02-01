f0a66d3bc5b69095939460a2f9738c8103cd5c5a
Thomas Mettler <thomas.mettler@lhep.unibe.ch>
2019-02-01 03:39:45 -0600

    Add a analyser module for the consolidatad output

---
+++ ubcrt/CRTXSEC/CRTNeutrinoIdBaseTool.h
@@ -0,0 +1,38 @@
+/**
+ *  @file   larpandora/LArPandoraEventBuilding/Slice.h
+ *
+ *  @brief  header for the lar pandora slice class
+ */
+
+#ifndef LAR_PANDORA_NEUTRINO_ID_BASE_TOOL_H
+#define LAR_PANDORA_NEUTRINO_ID_BASE_TOOL_H 1
+
+#include "art/Framework/Principal/Event.h"
+
+#include "larpandora/LArPandoraEventBuilding/Slice.h"
+#include "larpandora/LArPandoraInterface/LArPandoraHelper.h"
+
+namespace lar_pandora
+{
+
+/**
+ *  @brief  Abstract base class for a neutrino ID tool
+ */
+class CRTNeutrinoIdBaseTool
+{
+public:
+    virtual ~CRTNeutrinoIdBaseTool() noexcept = default;
+
+    /**
+     *  @brief  The tools interface function. Here the derived tool will classify the input slices
+     *
+     *  @param  slices the input vector of slices to classify
+     *  @param  evt the art event
+     */
+    virtual void ClassifySlices(SliceVector &slices, const art::Event &evt) = 0;
+    virtual void ClassifySlicesCRTHit(SliceVector &slices, const art::Event &evt, std::string m_crthitLabel) = 0;
+};
+
+} // namespace lar_pandora
+
+#endif // #ifndef LAR_PANDORA_NEUTRINO_ID_BASE_TOOL_H
