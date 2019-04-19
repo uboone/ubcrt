9782b30dfc9fbe111416d5adad785a7acac06b7d
Kyle Knoepfel <knoepfel@fnal.gov>
2019-04-19 08:26:34 -0500

    Remove unsupported explicit template instantiations in classes.h files.

--- ubcrt/UBCRTCosmicFilter/classes.h
+++ ubcrt/UBCRTCosmicFilter/classes.h
@@ -6,7 +6,3 @@
 #include "ubobj/CRT/CRTHit.hh"
 #include "lardataobj/RecoBase/OpFlash.h"
 
-template class art::Assns<crt::CRTHit,recob::OpFlash,void>;
-template class art::Assns<recob::OpFlash,crt::CRTHit,void>;
-template class art::Wrapper<art::Assns<crt::CRTHit,recob::OpFlash,void> >;
-template class art::Wrapper<art::Assns<recob::OpFlash,crt::CRTHit,void> >;
