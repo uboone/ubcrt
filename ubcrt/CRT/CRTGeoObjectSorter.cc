62574073d7a690ab898c5872b399b4eda385c481
Kyle Knoepfel <knoepfel@fnal.gov>
2023-03-07 10:19:10 -0600

    Accommodate changes due to GeometryCore/ChannelMapAlg separation.

--- ubcrt/CRT/CRTGeoObjectSorter.cc
+++ ubcrt/CRT/CRTGeoObjectSorter.cc
@@ -1,26 +1,25 @@
 #include "ubcrt/CRT/CRTGeoObjectSorter.hh"
 #include "larcorealg/Geometry/AuxDetGeo.h"
-#include "larcorealg/Geometry/AuxDetSensitiveGeo.h"
+
+#include "fhiclcpp/ParameterSet.h"
 
 #include <sstream>
 #include <string>
 
 namespace crt {
 
-  template<class DetType>
-  CRTGeoObjectSorter::SortFunctor<DetType>::SortFunctor(const CRTGeoObjectSorter& c):
-    host(c)
-  {
-
-  }
+  CRTGeoObjectSorter::CRTGeoObjectSorter(
+      fhicl::ParameterSet const& pSet):
+      fNModules(pSet.get<uint32_t>("NModules", 76)),
+      fNStripsPerModule(pSet.get<uint32_t>("NStripsPerModule", 16))
+  {}
   
-  template<class DetType>
-  bool CRTGeoObjectSorter::SortFunctor<DetType>::operator()(DetType const& d1, DetType const& d2)
+  bool CRTGeoObjectSorter::compareAuxDets(geo::AuxDetGeo const& ad1, geo::AuxDetGeo const& ad2) const
   {
-    std::string name1 = d1.Name();
-    std::string name2 = d2.Name();
+    std::string name1 = ad1.Name();
+    std::string name2 = ad2.Name();
     uint32_t mod1=0, mod2=0, strip1=0, strip2=0;
-    for(uint32_t i=0; i<host.GetNModules();++i){
+    for(uint32_t i=0; i<fNModules;++i){
       std::ostringstream stream;
       stream<<"Module_"<<i;
       if(name1.find(stream.str())!= std::string::npos) mod1 = i;
@@ -28,28 +27,20 @@ namespace crt {
     }
     if(mod1>mod2) return true;
     if(mod2>mod1) return false;
-    for(uint32_t i=0; i<host.GetNStripsPerModule();++i){
+    for(uint32_t i=0; i<fNStripsPerModule;++i){
       std::ostringstream stream;
       stream<<"strip_"<<i;
       if(name1.find(stream.str())!= std::string::npos) strip1 = i;
       if(name2.find(stream.str())!= std::string::npos) strip2 = i;
     }
-    if(strip1>strip2) return true;
+    return strip1>strip2;
+  }
+
+  bool CRTGeoObjectSorter::compareAuxDetSensitives(geo::AuxDetSensitiveGeo const& ads1,
+                                                   geo::AuxDetSensitiveGeo const& ads2) const
+  {
+    /// There is 1 SV per AD so the return value is irrelevant.
     return false;
-  }
-
-  CRTGeoObjectSorter::CRTGeoObjectSorter(
-      fhicl::ParameterSet const& pSet):
-      fNModules(pSet.get<uint32_t>("NModules", 76)),
-      fNStripsPerModule(pSet.get<uint32_t>("NStripsPerModule", 16)) 
-  {
-
-  }
-
-  void CRTGeoObjectSorter::SortAuxDets(AuxDetList& adgeo) const 
-  {
-    std::sort(adgeo.begin(), adgeo.end(), 
-              SortFunctor<geo::AuxDetGeo>(*this));
   }
 
 }  /// namespace crt
